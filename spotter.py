9cd5b1c4c5aec19c7224616566e324a8e4d65d31
Sam Clements <sam@borntyping.co.uk>
2013-05-04 20:20:41 +0100

    Refactored Spotter
    * Better example .spotter file, used for testing
    * Watch objects
    * 100% less regex!
    * More comments
    * 'watch-final' directive, for commands that should be the last to run

--- spotter.py
+++ spotter.py
@@ -25,96 +25,114 @@ def merge(*dictionaries):
         dictionary.update(d)
     return dictionary
 
+class Watch(object):
+    def __init__(self, pattern, command, final=False):
+        self.pattern = pattern
+        self.command = command
+        self.final = final
+
+    def pattern_matches(self, path):
+        """Return true if the given path matches the watch pattern"""
+        return fnmatch.fnmatch(path, self.pattern) or path == self.pattern
+
 class Spotter(pyinotify.ProcessEvent):
     INOTIFY_EVENT_MASK = pyinotify.IN_CREATE | pyinotify.IN_CLOSE_WRITE
-
-    RE_DEFINE  = r'define: (.*) -> (.*)'
-    RE_ENTRY   = r'(?:start|enter|begin): (.*)'
-    RE_WATCH   = r'watch: (.*) -> (.*)'
-    RE_EXIT    = r'(?:stop|exit|end): (.*)'
-    RE_COMMENT = r'^#'
+    
+    COMMANDS = ('define', 'start', 'watch', 'watch-final', 'stop')
 
     def __init__(self, filename=None):
         self.definitions = dict()
-
         self.entry_commands = list()
-        self.watches = collections.OrderedDict()
         self.exit_commands = list()
-
-        # Lines matching each regex are passed to it's paired function
-        self.regexes = [(re.compile(r, re.I), f) for r, f in [
-            (self.RE_DEFINE,  self.add_definition),
-            (self.RE_ENTRY,   self.entry_commands.append),
-            (self.RE_WATCH,   self.add_watch),
-            (self.RE_EXIT,    self.exit_commands.append),
-            (self.RE_COMMENT, None),
-        ]]
-
+        self.watches = list()
+        
         # Read in the configuration, if initialised with a filename
         if not filename is None:
             self.read_file(filename)
+
+    # --------------------------------------------------
+    # Initialisation
+    # --------------------------------------------------
+
+    def read_file(self, filename):
+        """Yield each command from a spotter config file"""
+        with open(filename, 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    self.read_line(line)
+
+    def read_line(self, line):
+        """Read in a single line and return a (directive, arguments) tuple"""
+        directive, arguments = line.split(':')
+        arguments = [a.strip() for a in arguments.split('->')]
+        self.read_directive(directive.strip(), *arguments)
+        
+    def read_directive(self, directive, *arguments):
+        """Read a parsed directive and add it to the current instance"""
+        try:
+            {
+                'define':      self.add_definition,
+                'start':       self.entry_commands.append,
+                'stop':        self.exit_commands.append,
+                'watch':       self.add_watch,
+                'watch-final': self.add_final_watch,
+            }[directive](*arguments)
+        except KeyError:
+            print("Unknown directive '{}'".format(directive))
+            raise
 
     def add_definition(self, key, value):
         self.definitions[key] = value
 
     def add_watch(self, pattern, command):
-        if not pattern in self.watches:
-            self.watches[pattern] = list()
-        self.watches[pattern].append(command)
-        # self.watches.append((pattern, command))
+        self.watches.append(Watch(pattern, command))
 
-    def read_file(self, filename):
-        """Read the watches and other options from a config file"""
-        with open(filename, 'r') as file:
-            for line in file:
-                line = line.strip()
-                if line:
-                    self.read_line(line)
+    def add_final_watch(self, pattern, command):
+        self.watches.append(Watch(pattern, command, final=True))
 
-    def read_line(self, line):
-        """Read in a single line"""
-        for regex, func in self.regexes:
-            match = regex.match(line)
-            if match and func is not None:
-                return func(*match.groups())
+    # --------------------------------------------------
+    # Running
+    # --------------------------------------------------
 
-    def run_command(self, command, **kwargs):
+    def run(self, command, **kwargs):
         """Run a single command
 
         The command is formatted using the stored definitions and any keyword
         arguments passed to the function."""
-        arguments = merge(self.definitions, kwargs)
-        subprocess.call(command.format(**arguments), shell=True)
-
-    def run_commands(self, commands, **kwargs):
-        """Run a list of commands"""
-        for command in commands:
-            self.run_command(command, **kwargs)
+        command = command.format(**merge(self.definitions, kwargs))
+        subprocess.call(command, shell=True)
 
     def loop(self):
+        """Run the inotify loop inside the context manager"""
+        with self:
+            self.inotify_loop()
+    
+    def inotify_loop(self):
         watch_manager = pyinotify.WatchManager()
         notifier = pyinotify.Notifier(watch_manager, self)
         watch_manager.add_watch('.', Spotter.INOTIFY_EVENT_MASK, rec=True)
-
-        with self:
-            notifier.loop()
-
-    def __enter__(self):
-        self.run_commands(self.entry_commands)
+        notifier.loop()
 
     def process_default(self, event):
         """Run the command associated with the event"""
-        for pattern, commands in self.watches.items():
-            path = os.path.relpath(event.pathname)
-            if fnmatch.fnmatch(path, pattern) or path == pattern:
-                return self.run_commands(commands, filename=event.pathname)
+        for watch in self.watches:
+            if watch.pattern_matches(os.path.relpath(event.pathname)):
+                self.run(watch.command, filename=event.pathname)
+                if watch.final == True:
+                    break
+
+    def __enter__(self):
+        for command in self.entry_commands:
+            self.run(command)
 
     def __exit__(self, type, value, traceback):
-        self.run_commands(self.exit_commands)
+        for command in self.exit_commands:
+            self.run(command)
 
 parser = argparse.ArgumentParser(description="Watch files for changes")
 parser.add_argument('-v', '--version', action='version', version="0.2")
-parser.add_argument('filename', default=".spotter",
+parser.add_argument('filename', nargs='?', default=".spotter",
     help="the spotter file to use")
 
 def main():
