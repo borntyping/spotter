8a516d80a832d05c407af7fdeb532dc68eb6957b
Sam Clements <sam@borntyping.co.uk>
2013-05-07 12:15:23 +0100

    Added setup.py and license

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'spotter',
+    version          = '1.0',
+    url              = "http://github.com/borntyping/spotter",
+
+    author           = "Sam Clements",
+    author_email     = "sam@borntyping.co.uk",
+
+    description      = "A command line tool for watching files and running shell commands when they change.",
+    long_description = open('README.rst').read(),
+
+    classifiers      = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Software Development :: Testing',
+        'Topic :: System :: Monitoring',
+        'Topic :: Utilities',
+    ],
+
+    packages         = find_packages(),
+
+    entry_points     = {
+        'console_scripts': [
+            'spotter = spotter:main',
+        ]
+    },
+
+    install_requires = [
+        'pyinotify==0.9.4',
+    ],
+)
